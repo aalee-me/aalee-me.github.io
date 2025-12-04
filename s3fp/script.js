c53e1d0f93a6c77d362366262f102806da9969f3
aalee-me <alee15@sas.upenn.edu>
2025-12-04 14:44:45 -0500

    Add files via upload

--- s3fp/script.js
+++ s3fp/script.js
@@ -1,6 +1,7 @@
 let shards = [];
 let neonColors = ['#00FFFF', '#FF00FF', '#FF3300', '#f2ff00ff', '#FFFFFF'];
-
+let folderElement; // We only need one variable for the folder
+let canvas;
 
 function setup() {
     canvas = createCanvas(windowWidth, windowHeight);
@@ -8,15 +9,40 @@ function setup() {
     frameRate(24); 
     noStroke();
     
+    // FIX: Changed .folder to #folder to match your HTML ID
     folderElement = document.querySelector('#folder');
+
+    // 3D Tilt Logic
+    if (folderElement) {
+        folderElement.addEventListener('mousemove', (e) => {
+            const rect = folderElement.getBoundingClientRect();
+            const centerX = rect.left + rect.width / 2;
+            const centerY = rect.top + rect.height / 2;
+            const mouseX = e.clientX - centerX;
+            const mouseY = e.clientY - centerY;
+            
+            // Calculate rotation
+            const rotateX = -(mouseY / 20); 
+            const rotateY = mouseX / 20;
+        
+            // Apply the style
+            folderElement.style.transform = `perspective(1000px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+        });
+
+        // Reset when mouse leaves
+        folderElement.addEventListener('mouseleave', () => {
+            folderElement.style.transform = `perspective(1000px) rotateX(0deg) rotateY(0deg)`;
+        });
+    }
 }
 
 function draw() {
     clear();
 
-    let bounds = folderElement.getBoundingClientRect();
-    
+    // Safety check
+    if (!folderElement) return;
 
+    let bounds = folderElement.getBoundingClientRect();
     let inbound = (mouseX > bounds.left && mouseX < bounds.right && mouseY > bounds.top && mouseY < bounds.bottom);
     
     if (inbound) {
@@ -26,9 +52,7 @@ function draw() {
 
     for (let num = shards.length - 1; num >= 0; num--) {
         let any = shards[num];
-        
         any.display();
-
         if (any.alpha <= 0) {
             shards.splice(num, 1);
         }
@@ -43,7 +67,6 @@ class Shard {
     constructor(x, y) {
         this.x = x;
         this.y = y;
-        
         this.color = random(neonColors);
         this.alpha = 255; 
         this.size = random(200, 300);
@@ -58,21 +81,17 @@ class Shard {
 
     display() {
         this.alpha = this.alpha - 40;
-
         push();
         translate(this.x, this.y);
-
         let neon = color(this.color);
         neon.setAlpha(this.alpha);
         fill(neon);
-        
         beginShape();
         vertex(0, 0);
         vertex(this.x1, this.y1);
         vertex(this.x2, this.y2);
         vertex(this.x3, this.y3);
         endShape(CLOSE);
-        
         pop();
     }
 }
